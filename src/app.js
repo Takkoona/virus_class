6b6e3b7842a5c6e0c1c77ef47f2c2eab5208cd3d
Chengyang Ji <chengyang.ji12@alumni.xjtlu.edu.cn>
2021-02-25 17:12:11 +0800

    First bar chart attempt

--- src/app.js
+++ src/app.js
@@ -1,7 +1,59 @@
 const d3 = require('d3');
-const data = require('./data/virusInfo.json');
+let data = require('./data/virusInfo.json')
 
-let scale = d3.scaleLinear().range([1, 100]);
-console.log(scale.range());
+const xValue = d => d.genomeNum;
+const yValue = d => d.virusName;
 
-// let virusClassPlot = d3.select('virusClassPlot')
+data = data.filter(virus => xValue(virus) >= 1000).sort((a, b) => {
+    return xValue(b) - xValue(a);
+});
+
+const outerWidth = 600;
+const outerHeight = 500;
+const margin = { left: 275, top: 30, right: 30, bottom: 30 }
+const barPadding = 0.2
+
+const innerWidth = outerWidth - (margin.left + margin.right);
+const innerHeight = outerHeight - (margin.top + margin.bottom);
+
+const svg = d3.select('#virusClassPlot')
+    .append('svg')
+    .attr('width', outerWidth)
+    .attr('height', outerHeight)
+
+const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => xValue(d))])
+    .range([0, innerWidth]);
+const yScale = d3.scaleBand()
+    .domain(data.map(d => yValue(d)))
+    .range([0, innerHeight])
+    .padding(barPadding);
+
+const xAxis = d3.axisBottom(xScale)
+    .ticks(5)
+    .tickSizeOuter(0);
+const yAxis = d3.axisLeft(yScale)
+    .tickSizeOuter(0);
+
+const barChartG = svg.append('g')
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); // Adjust plot inside the svg
+
+const xAxisG = barChartG.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + innerHeight + ")");
+const yAxisG = barChartG.append("g")
+    .attr("class", "y axis");
+
+xAxisG.call(xAxis);
+yAxisG.call(yAxis);
+
+barChartG.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("fill", "steelblue")
+    .attr("y", d => yScale(yValue(d)))
+    .attr("width", d => xScale(xValue(d)))
+    .attr("height", yScale.bandwidth())
+    .exit()
+    .remove();
